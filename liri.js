9a4656602cb0148dacc7182755e5131b44170800
Donald <donaldwhite84@gmail.com>
2018-05-17 18:09:29 -0600

    two

--- liri.js
+++ liri.js
@@ -5,14 +5,35 @@
 
 var request = require("request");
 
-var movieINfo = process.argv[2];
+var movieInfo = process.argv;
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieINfo + "&y=&plot=short&apikey=trilogy";
+var movieName = "";
+
+for (var i =2; i < movieInfo.length; i++){
+
+    if(i > 2 && i < movieInfo.length){
+
+        movieName = movieName + "+" + movieInfo[i];
+    }
+    else{
+        movieName += movieInfo[i];
+    }
+}
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieInfo + "&y=&plot=short&apikey=trilogy";
 
 // console.log(queryUrl);
 
 request(queryUrl, function(error, response, body) {
+    
   if (!error && response.statusCode === 200) {
-    console.log("Released: " + JSON.parse(body).Released);
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Year: " + JSON.parse(body).Year);
+    console.log("IMBD Rating: " + JSON.parse(body).imdbRating);
+    // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[2]);  
+    console.log("Country: " + JSON.parse(body).Country);
+    console.log("Language   " + JSON.parse(body).Language);
+    console.log("Plot: " + JSON.parse(body).Plot);
+    console.log("Actors: " + JSON.parse(body).Actors);
   }
 });
