a1d729f200322e32a35b1e1dd9629de77f7d9f2b
Donald <donaldwhite84@gmail.com>
2018-05-20 12:45:59 -0600

    052018-1

--- liri.js
+++ liri.js
@@ -1,26 +1,45 @@
 // require("dotenv").config();
 
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
-
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+var keys = require('./keys.js');
 var request = require("request");
+var twitter = require('twitter');
+var spotify = require('spotify');
+var fs = require('fs');
 
-var movieInfo = process.argv;
+var nodeArgv = process.argv;
+var command = process.arg[2];
 
-var movieName = "";
+var movSong = "";
 
-for (var i =2; i < movieInfo.length; i++){
+for(var i = 3; i < nodeArgv.length; i++){
+    if(i>3 && i < nodeArgv.length){
+        movSong = movSong + "+" + nodeArgv[i];
 
-    if(i > 2 && i < movieInfo.length){
-
-        movieName = movieName + "+" + movieInfo[i];
-    }
-    else{
-        movieName += movieInfo[i];
+    }else{
+        movSong = movSong + nodeArgv[i];
     }
 }
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieInfo + "&y=&plot=short&apikey=trilogy";
+//Switch Case
+
+
+
+var movieName = "";
+
+for (var i =2; i < command.length; i++){
+
+    if(i > 2 && i < command.length){
+
+        movieName = movieName + "+" + command[i];
+    }
+    else{
+        movieName += command[i];
+    }
+}
+
+var queryUrl = "http://www.omdbapi.com/?t=" + command + "&y=&plot=short&apikey=trilogy";
 
 // console.log(queryUrl);
 
@@ -30,7 +49,9 @@ request(queryUrl, function(error, response, body) {
     console.log("Title: " + JSON.parse(body).Title);
     console.log("Year: " + JSON.parse(body).Year);
     console.log("IMBD Rating: " + JSON.parse(body).imdbRating);
-    // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[2]);  
+    // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[2].Source); 
+     // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[2].Value);  
+
     console.log("Country: " + JSON.parse(body).Country);
     console.log("Language   " + JSON.parse(body).Language);
     console.log("Plot: " + JSON.parse(body).Plot);
